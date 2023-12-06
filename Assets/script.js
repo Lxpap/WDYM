2fde2aa3b208cb99c245b962a4f2be0140cc2807
Alex Papadakis <Lx.papad@gmail.com>
2023-12-06 14:40:51 +0000

    Adds JS functionality to fetch results and hide Gif when results are in

--- Assets/script.js
+++ Assets/script.js
@@ -1,35 +1,103 @@
 
+    // Function that randomises GIF on main page before using search
+    
     async function getRandomGif() {
-        try {
-            var response = await fetch(`https://api.giphy.com/v1/gifs/search?q=wdym&api_key=[GIPHYKEY]`);
-            var data = await response.json();
+        var response = await fetch(`https://api.giphy.com/v1/gifs/search?q=wdym&api_key=OBXwlhFG7x9ODt6AZN21VX3uccqdMBxK`);
+        var data = await response.json();
      
-            var randomIndex = Math.floor(Math.random() * data.data.length);
-            var randomGif = data.data[randomIndex];
+        var randomIndex = Math.floor(Math.random() * data.data.length);
+        var randomGif = data.data[randomIndex];
      
-            return randomGif.images.original.url;
-        } catch (error) {
-            console.error('Error retrieving random GIF:', error);
-            throw new Error('Failed to retrieve random GIF');
-        }
+        return randomGif.images.original.url;
+        
     }
     
     async function displayGif() {
-        try {
-            var gifUrl = await getRandomGif();
-            var randomGifElement = document.getElementById('gif');
+        var gifUrl = await getRandomGif();
+        var randomGifElement = document.getElementById('gif');
             randomGifElement.src = gifUrl;
-        } catch (error) {
-            console.error('Error displaying random GIF:', error.message);
-        }
+        
     }
 
     getRandomGif()
-        .then((gifUrl) => {
-            console.log('Random GIF URL:', gifUrl);
-        })
-        .catch((error) => {
-            console.error('Error:', error.message);
-        });
-
     displayGif();
+
+// Search Functions
+
+
+    function search() {
+        var searchTerm = $('#searchInput').val();
+    
+        if (!searchTerm) {
+            alert('Please enter a search term.');
+            return;
+        }
+    
+        $.when(
+            fetchUrbanDictionaryResults(searchTerm),
+            fetchDictionaryResults(searchTerm),
+            fetchGoogleResults(searchTerm)
+        ).done(function(urbanDictionaryResults, dictionaryResults, googleResults) {
+            
+            $('#randomGif').hide();
+            $('#urbanDictionaryResults, #dictionaryResults, #googleResults').show();
+    
+            displayResults('urbanDictionaryResults', 'Urban Dictionary', urbanDictionaryResults[0].list);
+            displayResults('dictionaryResults', 'Dictionary', dictionaryResults[0]);
+            displayResults('googleResults', 'Google', googleResults[0].items);
+        }).fail(function(error) {
+            console.error('Error fetching search results:', error);
+        });
+    }
+    
+  
+    function displayResults(containerId, title, results) {
+        var resultBox = $('<div>').addClass('result-box');
+        resultBox.append(`<h3>${title} Results</h3>`);
+    
+        if (Array.isArray(results)) {
+            var resultItems = results.map(item => {
+                
+                var content = item.definition || item.text || item; 
+                return `<li>${content}</li>`;
+            });
+    
+            resultBox.append($('<ul>').html(resultItems.join('')));
+        } else {
+            
+            resultBox.append(`<p>${results}</p>`);
+        }
+    
+        
+        $(`#${containerId}`).empty().append(resultBox);
+    }
+    
+    
+    function fetchUrbanDictionaryResults(searchTerm) {
+        var apiUrl = `https://api.urbandictionary.com/v0/define?term=${searchTerm}`;
+        return $.ajax({
+            url: apiUrl,
+            method: 'GET',
+            dataType: 'json'
+        });
+    }
+    
+    function fetchDictionaryResults(searchTerm) {
+        var apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`;
+        return $.ajax({
+            url: apiUrl,
+            method: 'GET',
+            dataType: 'json'
+        });
+    }
+    
+    function fetchGoogleResults(searchTerm) {
+        var apiUrl = `https://www.googleapis.com/customsearch/v1?q=${searchTerm}&key=[GOOGLEAPIKEY]&cx=BROWSERID`;
+        return $.ajax({
+            url: apiUrl,
+            method: 'GET',
+            dataType: 'json'
+        });
+    }
+
+    
